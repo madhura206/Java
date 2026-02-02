fdbfe1978b4f39aab69a9647ff8173ddf637a5e1
madhura206 <madhurahawelikar7@gmail.com>
2026-02-02 21:37:24 +0530

    while Loop Added Successfully

---
+++ src/com/company/While_Loop.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class While_Loop {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i < 10){
+            System.out.println("Hello Everyone");
+            i++;
+        }
+        while(true){     //While loop is an infinite loop
+            System.out.println("I am an infinite while loop");
+        }
+
+        // Practice
+//        while(i <= 100){
+//            System.out.println(i);
+//            i++;
+//        }
+    }
+}
