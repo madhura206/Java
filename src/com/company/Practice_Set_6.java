f962ec864ff809b75c213295b11d0194e97ed1f0
madhura206 <madhurahawelikar7@gmail.com>
2026-02-13 19:21:33 +0530

    Practice Set on Loops is Updated Successfully

--- src/com/company/Practice_Set_6.java
+++ src/com/company/Practice_Set_6.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Practice_Set_6 {
     public static void main(String[] args) {
         //Problem 1:
-        for(int i = 4; i > 0 ; i--){
+        for (int i = 4; i > 0; i--) {
             for(int j = 0; j < i; j++){
                 System.out.print("* ");
 
@@ -12,7 +12,7 @@ public class Practice_Set_6 {
             System.out.println();
         }
 
-        // Problem 2:
+            // Problem 2:
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of n: ");
         int num = sc.nextInt();
@@ -26,20 +26,42 @@ public class Practice_Set_6 {
         System.out.print("The sum of first n even numbers is: ");
         System.out.println(sum);
 
-        //Problem 3:
+            //Problem 3:
         System.out.print("Enter the number: ");
         int num1 = sc.nextInt();
         for(i = 1; i <= 10; i++){
             System.out.println(num1 + "x" + i + "=" + (num1 * i));
         }
 
-        //Problem 4:
+            //Problem 4:
         System.out.println("Multiplication table in reverse order");
         System.out.print("Enter the number: ");
         int num2 = sc.nextInt();
         for(i = 10; i > 0; i--){
             System.out.println(num2 + "X" + i + "=" + num2 * i);
         }
+
+            //Problem 5:
+        System.out.println("Enter number: ");
+        int num3 = sc.nextInt();
+
+        int pro = 1;
+        i = 1;
+        while(i <= num3){
+            pro = pro * i;
+            i++;
+        }
+        System.out.println(pro);
+
+            // Problem 6:
+            System.out.println("Sum of the multiples of 8");
+            int num4 = 8;
+            int sum1 = 0;
+            for (i = 1; i <= 10; i++) {
+                sum1 = sum1 + (num4 * i);
+
+            }
+        System.out.println(sum1);
 
 
 
