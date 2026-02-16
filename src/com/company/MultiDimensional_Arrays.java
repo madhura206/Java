be3154b627d03375f9e316b782b66264e496f3b5
madhura206 <madhurahawelikar7@gmail.com>
2026-02-16 21:14:32 +0530

    Learned Multidimensional Arrays

---
+++ src/com/company/MultiDimensional_Arrays.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class MultiDimensional_Arrays {
+    public static void main(String[] args)  {
+        int [][] flats; // 2-D Array
+        flats = new int[2][3];
+        flats[0][0] = 101;
+        flats[0][1] = 102;
+        flats[0][2] = 103;
+        flats[1][0] = 104;
+        flats[1][1] = 105;
+        flats[1][2] = 106;
+
+        System.out.println("Printing a 2-D Array using for loop");
+        for(int i = 0; i < flats.length; i++){
+            for(int j = 0; j < flats[i].length; j++){
+                System.out.print(flats[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+
+}
