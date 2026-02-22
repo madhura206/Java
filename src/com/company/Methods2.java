e3aac9da51b82074a2e71b06f59d7da39c7bd7e1
madhura206 <madhurahawelikar7@gmail.com>
2026-02-22 21:53:43 +0530

    Methods in Java Added Successfully

---
+++ src/com/company/Methods2.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Methods2 {
+    static int logic(int x, int y){
+        int z;
+        if(x > y){
+            z = x + y;
+        }
+        else{
+            z = (x + y) * 5;
+        }
+        x = 566; //this does not affect the values of a amd b.
+        return z;
+    }
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c;
+//        Method invocation using object creation
+//        Methods2 obj = new Methods2();
+//        c = obj.logic(a , b);
+        c = logic(a , b); // Here, c == z
+
+        int a1 = 2;
+        int b1 = 1;
+        int c1;
+        c1 = logic(a1, b1); // Here, c1 == z
+        System.out.println(c);
+        System.out.println(c1);
+
+    }
+}
