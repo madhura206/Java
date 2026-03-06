b06d0de97dabfdd67a5abe134132b5649cc56021
madhura206 <madhurahawelikar7@gmail.com>
2026-03-06 21:53:01 +0530

    Practice Updated

--- src/com/company/Practice7_Arrays.java
+++ src/com/company/Practice7_Arrays.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class Practice7_Arrays {
     public static void main(String[] args) {
         // Problem 1:
-        Scanner sc = new Scanner(System.in);
-        float [] marks;
-        marks = new float[5];
-
-        for(int i = 0; i <5; i++){
-            System.out.print("Enter value " + i + ":");
-            marks[i] = sc.nextFloat();
-        }
-
-        float sum = 0;
-        for(int i = 0; i < marks.length; i++){
-            sum = sum + marks[i];
-        }
-        System.out.println(sum);
+//        Scanner sc = new Scanner(System.in);
+//        float [] marks;
+//        marks = new float[5];
+//
+//        for(int i = 0; i <5; i++){
+//            System.out.print("Enter value " + i + ":");
+//            marks[i] = sc.nextFloat();
+//        }
+////
+//        float sum = 0;
+//        for(int i = 0; i < marks.length; i++){
+//            sum = sum + marks[i];
+//        }
+//        System.out.println(sum);
 
         //Problem 2: Taken same array as above
 //        float [] marks;
@@ -47,20 +47,85 @@ public class Practice7_Arrays {
 //        }
 
         //Problem 3:
-        float [] marks1;
-        marks1 = new float[5];
+//        float [] marks1;
+//        marks1 = new float[5];
+//
+//        for(int i = 0; i <5; i++){
+//            System.out.print("Enter value " + i + ":");
+//            marks1[i] = sc.nextFloat();
+//        }
+//
+//        float sum1 = 0;
+//        for(int i = 0; i < marks1.length; i++){
+//            sum = sum + marks1[i];
+//        }
+//        float average = sum / 5;
+//        System.out.println("Average in physics is: " + average);
 
-        for(int i = 0; i <5; i++){
-            System.out.print("Enter value " + i + ":");
-            marks1[i] = sc.nextFloat();
+        //Problem 4:
+//        int [][] mat1 = {{1, 2, 3},
+//                         {4, 5, 6}};
+//        int [][] mat2 = {{2, 6, 13},
+//                         {3, 7, 1}};
+//        int [][] res = {{0, 0, 0},
+//                        {0, 0, 0}};
+//
+//        for(int i = 0; i < mat1.length; i++){  // row number of times
+//            for(int j = 0; j < mat2[i].length ; j++){  // column number of times
+//                res[i][j] = mat1[i][j] + mat2[i][j];
+//                System.out.print(res[i][j] + " ");
+//            }
+//            System.out.println();
+//
+//        }
+
+        // Problem 5:
+//        int [] arr;
+//        arr = new int [5];
+//        for(int i = 0; i < arr.length; i++){
+//            System.out.format("Enter element %d: ", i);
+//            arr[i] = sc.nextInt();
+//        }
+//
+//        //Reversed Array
+//        for(int i = arr.length - 1; i >= 0; i--){
+//            System.out.print(arr[i] + " ");
+//        }
+
+        // Alternative method [Swapping]
+        int [] arr = {1,2,3,4,5,6};
+        int l = arr.length;
+//        int n = Math.floorDiv(l, 2);
+//        int temp;
+//
+//        for(int i = 0; i < n; i++){
+//            // Swap arr[i] and arr[l-i-1]
+//            temp = arr[i];
+//            arr[i] = arr[l-i-1];
+//            arr[l-i-1] = temp;
+//        }
+//        for(int element:arr){
+//            System.out.print(element + " ");
+//        }
+
+        //Problem 6:
+        // Using array from Problem 1
+//int [] arr = {1,2,3,4,5,6};
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length; j++){
+                if(arr[i] > arr[j]){
+                    System.out.print(arr[j]);
+                }
+            }
         }
 
-        float sum1 = 0;
-        for(int i = 0; i < marks1.length; i++){
-            sum = sum + marks1[i];
-        }
-        float average = sum / 5;
-        System.out.println("Average in physics is: " + average);
+
+
+
+
+
+
+
 
         }
     }
